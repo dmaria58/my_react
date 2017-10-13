cd390740614c6356bc2e006399cb78d0b87783f3
dmaria58 <linaninani@163.com>
2017-10-13 16:09:33 +0800

    style特殊处理

--- build/Dom/reactDOMComponent.js
+++ build/Dom/reactDOMComponent.js
@@ -1,14 +1,37 @@
 'use strict';
-/*
-*style、function单独处理
-**/
+/**
+* 说明：生成dom node
+* mountComponent：把虚拟 dom 的节点生成成具体的 dom node
+* _updateDomprops:处理特殊props,如style,function
+*/
 var ReactDOMComponent =  {
+  _updateDomprops:function(_props){
+    var props=_props;
+    var _myprops={};
+    for(var key in props){
+      switch(key){
+        case 'style':
+          var _styleprops=props[key];
+          var _styledetail="";
+          for(var _key in _styleprops){
+            _styledetail+=_key+":"+_styleprops[_key];
+          }
+          _myprops[key]=_styledetail;
+        break;
+        default:
+          _myprops[key]=props[key];
+        break;
+      }
+    }
+    return _myprops;
+  },
   mountComponent:function(_currentElement) {
     var result;
     var props = _currentElement.props;
     var _this=this;
     if ( _currentElement.type) {
       result = document.createElement( _currentElement.type );
+      props=_this._updateDomprops(props);
       for(var key in props ) {
         if(key!="children"){
            result.setAttribute( key, props[ key ] );
